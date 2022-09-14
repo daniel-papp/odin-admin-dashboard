9d9609f17484cc50b960c4ebe90723766138ef10
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-09-14 08:01:33 +0200

    Add collapse function to dashboard

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const dashboard = document.querySelector(".dashboard");
 
-dashboard.addEventListener('click', ()=> console.log('collapse'));
+dashboard.addEventListener('click', ()=> collapseDashboard());
+
+let collapsed = false;
+
+function collapseDashboard() {
+    const body = document.querySelector('body');
+    const sidebar = document.querySelector('.sidebar');
+
+    if (!collapsed) {
+        body.classList.add('collapsed');
+        sidebar.classList.add('collapsed');
+        collapsed = true;
+    } else {
+        body.classList.remove('collapsed');
+        sidebar.classList.remove('collapsed');
+        collapsed = false;
+    }
+}
